2298bcc280cf89a8f4e7d141ba94f7818901ce1c
daviidy <yaodavidarmel@gmail.com>
2020-10-15 14:09:13 +0000

    add changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,18 @@
 def bubble_sort(arr)
-    #we get the length of the array
-    arr_length = arr.length
-
-    loop do
-
-      #we create a variable taht will be checked if elements are swapped
-      is_swapped = false
-
-      (arr_length - 1).times do |i|
-        puts "iteration is #{i}"
-        if arr[i] > arr[i + 1]
-          arr[i] = arr[i + 1]
-          arr[i + 1] = arr[i]
-          is_swapped = true
-        end
-      end
-      break if not is_swapped
+  # we get the length of the array
+  arr_length = arr.length
+  loop do
+    # we create a variable taht will be checked if elements are swapped
+    is_swapped = false
+    (arr_length - 1).times do |i|
+      next unless arr[i] > arr[i + 1]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = arr[i]
+      is_swapped = true
     end
-   
-    arr
+    break if !is_swapped
+  end
+  arr
 end
 
 arr = [8, 3, 1, 6, 5, 9, 2]
@@ -26,21 +20,20 @@ arr = [8, 3, 1, 6, 5, 9, 2]
 print bubble_sort(arr)
 
 def bubble_sort_by(arr)
-    sorted = false
-    until sorted
-        sorted = true
-        0.upto(arr.length-2) do |i|
-            p i
-            sorted_blk = yield(arr[i], arr[i+1])
-            if sorted_blk > 0
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                sorted = false
-            end
-        end
-    end
-    p arr
-end 
-
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
-       left.length - right.length
+  sorted = false
+  until sorted
+    sorted = true
+    0.upto(arr.length - 2) do |i|
+      sorted_blk = yield(arr[i], arr[i + 1])
+      if sorted_blk.positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        sorted = false
       end
+    end
+  end
+  p arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left,right|
+  left.length - right.length
+end
